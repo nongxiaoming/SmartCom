223a99f8a7a26bb78b62378ea8106bb6b142e0da
nongxiaoming <nongxiaoming@gmail.com>
2014-06-16 21:53:32 +0800

    remove the dialog which not used.

--- SmartCom/resource.h
+++ SmartCom/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by CometBrowser.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SmartCom.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
@@ -17,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
